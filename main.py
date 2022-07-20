a8dcc2c2161115ce8fbcd1a71484ca8a22b87a7c
Andrey Bushev <morgan9757@gmail.com>
2022-07-20 20:42:54 +0300

    init

---
+++ main.py
@@ -0,0 +1,23 @@
+def print_4():
+    print(4)
+
+
+def print_3():
+    print(3)
+
+
+def print_2():
+    print(2)
+
+
+def print_1():
+    print(1)
+
+def print_Hello():
+    print("Hello")
+
+def print_Hello_World():
+    print("Hello World!")
+
+if __name__ == "main":
+    print_Hello_World()
